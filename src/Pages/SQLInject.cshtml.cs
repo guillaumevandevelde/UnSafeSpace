61a440997f0bcf5c06b2810b98fec2ca0125c15a
guillaumevandevelde <Guillaume.vandevelde@district09.gent>
2023-12-03 17:04:20 +0100

    project init

---
+++ src/Pages/SQLInject.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UnSafeSpace.Pages
+{
+    public class SQLInjectModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
